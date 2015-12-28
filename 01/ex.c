75198a33c0a0c60bbd7e16d18a94602edf62e39e
Jiri Slaby <jirislaby@gmail.com>
2015-12-28 14:47:58 +0100

    01: more updates
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- 01/ex.c
+++ 01/ex.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 #include "x.h"
 
 #ifdef X
@@ -9,7 +10,16 @@
 # error X undefined
 #endif
 
+#define my_macro(x, y)		x
+#define my_macro2(x, y)		"TODO"
+
 int main()
 {
+	puts(my_macro(__FILE__, je));
+	puts(my_macro(__DATE__, je));
+	puts(my_macro(__TIME__, je));
+
+	puts(my_macro(my_macro2(TIME, STAMP), je));
+
 	return 0;
 }
