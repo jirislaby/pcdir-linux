3323e1e6fdddf053d392480807442406ee8993e4
Jiri Slaby <jirislaby@gmail.com>
2016-01-03 11:49:09 +0100

    09: add
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

---
+++ 09/main.c
@@ -0,0 +1,14 @@
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#ifdef GO_LONG
+static const struct option opts[] = {
+};
+#endif
+
+int main(int argc, char **argv, char **envp)
+{
+	return 0;
+}
