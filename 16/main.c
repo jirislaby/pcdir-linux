00382a5fcc7ebad40c968faf17b458dfb8e21e00
Jiri Slaby <jirislaby@gmail.com>
2016-01-05 12:56:28 +0100

    16: add
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

---
+++ 16/main.c
@@ -0,0 +1,15 @@
+#include <err.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
